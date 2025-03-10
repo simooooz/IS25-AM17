79e6f57c6e88901be0aab1d6b9fa01c22e28caf6
Davide Cutrupi <davidecutrupi2@gmail.com>
2025-03-10 12:57:40 +0100

    Split insert into insert and check methods and add destroy and support methods (getLinkedNeighbors and connectors support methods)

--- src/main/java/it/polimi/ingsw/model/components/Component.java
+++ src/main/java/it/polimi/ingsw/model/components/Component.java
@@ -3,21 +3,15 @@ package it.polimi.ingsw.model.components;
 import it.polimi.ingsw.model.components.utils.ConnectorType;
 import it.polimi.ingsw.model.player.Ship;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Component {
 
     private ConnectorType[] connectors;
-    private int x;
-    private int y;
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
+    protected int x;
+    protected int y;
 
     public Component(ConnectorType[] connectors) {
         this.connectors = connectors;
@@ -33,45 +27,64 @@ public class Component {
         return rowDiff + colDiff == 1;
     }
 
-    public void insertComponent(Ship ship, int row, int col) throws Exception {
-        if ((row < 0 || row > 4) || (col < 0 || col > 6) || (row == 0 && col == 0) || (row == 0 && col == 1) || (row == 0 && col == 3) || (row == 0 && col == 5) || (row == 0 && col == 6) || (row == 1 && col == 0) || (row == 1 && col == 6) || (row == 4 && col == 3))
-            throw new Exception(); // Check if position is valid
-        if (ship.getDashboard()[row][col].isPresent()) throw new Exception();
+    public List<Component> getLinkedNeighbors(Ship ship) {
+        List<Component> neighbors = new ArrayList<>();
+        ship.getDashboard(y-1, x).ifPresent(n -> {
+            if (areConnectorsLinked(connectors[0], n.connectors[2]))
+                neighbors.add(n);
+        });
+        ship.getDashboard(y+1, x).ifPresent(n -> {
+            if (areConnectorsLinked(connectors[2], n.connectors[0]))
+                neighbors.add(n);
+        });
+        ship.getDashboard(y, x-1).ifPresent(n -> {
+            if (areConnectorsLinked(connectors[3], n.connectors[1]))
+                neighbors.add(n);
+        });
+        ship.getDashboard(y, x+1).ifPresent(n -> {
+            if (areConnectorsLinked(connectors[1], n.connectors[3]))
+                neighbors.add(n);
+        });
+        return neighbors;
+    }
 
-        if ( // Not isolated check
-                !(col > 0 && ship.getDashboard()[row][col - 1].isPresent()) &&
-                        !(row > 0 && ship.getDashboard()[row - 1][col].isPresent()) &&
-                        !(col < 6 && ship.getDashboard()[row][col + 1].isPresent()) &&
-                        !(row < 4 && ship.getDashboard()[row + 1][col].isPresent())
-        ) throw new Exception();
-        if ( // Left connectors check
-                col > 0 && ship.getDashboard()[row][col - 1].isPresent() &&
-                        ship.getDashboard()[row][col - 1].get().connectors[1] != ship.getDashboard()[row][col].get().connectors[3] &&
-                        ((ship.getDashboard()[row][col - 1].get().connectors[1] != ConnectorType.UNIVERSAL && ship.getDashboard()[row][col].get().connectors[3] != ConnectorType.UNIVERSAL) ||
-                                (ship.getDashboard()[row][col - 1].get().connectors[1] == ConnectorType.EMPTY || ship.getDashboard()[row][col].get().connectors[3] == ConnectorType.EMPTY))
-        ) throw new Exception();
-        if ( // Top connectors check
-                row > 0 && ship.getDashboard()[row - 1][col].isPresent() &&
-                        ship.getDashboard()[row - 1][col].get().connectors[2] != ship.getDashboard()[row][col].get().connectors[0] &&
-                        ((ship.getDashboard()[row - 1][col].get().connectors[2] != ConnectorType.UNIVERSAL && ship.getDashboard()[row][col].get().connectors[0] != ConnectorType.UNIVERSAL) ||
-                                (ship.getDashboard()[row - 1][col].get().connectors[2] == ConnectorType.EMPTY || ship.getDashboard()[row][col].get().connectors[0] == ConnectorType.EMPTY))
-        ) throw new Exception();
-        if ( // Right connectors check
-                col < 6 && ship.getDashboard()[row][col + 1].isPresent() &&
-                        ship.getDashboard()[row][col + 1].get().connectors[3] != ship.getDashboard()[row][col].get().connectors[1] &&
-                        ((ship.getDashboard()[row][col + 1].get().connectors[3] != ConnectorType.UNIVERSAL && ship.getDashboard()[row][col].get().connectors[1] != ConnectorType.UNIVERSAL) ||
-                                (ship.getDashboard()[row][col + 1].get().connectors[3] == ConnectorType.EMPTY || ship.getDashboard()[row][col].get().connectors[1] == ConnectorType.EMPTY))
-        ) throw new Exception();
-        if ( // Bottom connectors check
-                row < 4 && ship.getDashboard()[row + 1][col].isPresent() &&
-                        ship.getDashboard()[row + 1][col].get().connectors[0] != ship.getDashboard()[row][col].get().connectors[2] &&
-                        ((ship.getDashboard()[row + 1][col].get().connectors[0] != ConnectorType.UNIVERSAL && ship.getDashboard()[row][col].get().connectors[2] != ConnectorType.UNIVERSAL) ||
-                                (ship.getDashboard()[row + 1][col].get().connectors[0] == ConnectorType.EMPTY || ship.getDashboard()[row][col].get().connectors[2] == ConnectorType.EMPTY))
-        ) throw new Exception();
+    public static boolean areConnectorsCompatible(ConnectorType conn1, ConnectorType conn2) {
+        if (conn1 == conn2) return true;
+        return conn1 == ConnectorType.UNIVERSAL && conn2 != ConnectorType.EMPTY || conn2 == ConnectorType.UNIVERSAL && conn1 != ConnectorType.EMPTY;
+    }
+
+    public static boolean areConnectorsLinked(ConnectorType conn1, ConnectorType conn2) {
+        return areConnectorsCompatible(conn1, conn2) && conn1 != ConnectorType.EMPTY && conn2 != ConnectorType.EMPTY;
+    }
+
+    public void affectDestroy(Ship ship) {
+        ship.getDashboard(y, x).ifPresent(ship.getDiscards()::add);
+        ship.getDashboard()[y][x] = Optional.empty();
+    }
+
+    public void insertComponent(Ship ship, int row, int col) throws Exception {
+        if ((row < 0 || row > 4) || (col < 0 || col > 6) || (row == 0 && col == 0) || (row == 0 && col == 1) || (row == 0 && col == 3) || (row == 0 && col == 5) || (row == 0 && col == 6) || (row == 1 && col == 0) || (row == 1 && col == 6) || (row == 4 && col == 3)) throw new Exception(); // Check if position is valid
+        if (ship.getDashboard()[row][col].isPresent()) throw new Exception();
 
         this.x = row;
         this.y = col;
         ship.getDashboard()[row][col] = Optional.of(this);
+    }
+
+    public void checkComponent(Ship ship) throws Exception {
+        if ( // Not isolated check
+            ship.getDashboard(y, x-1).isEmpty() &&
+            ship.getDashboard(y-1, x).isEmpty() &&
+            ship.getDashboard(y, x+1).isEmpty() &&
+            ship.getDashboard(y+1, x).isEmpty()
+        ) throw new Exception();
+
+        if ( // Compatible connectors check
+            ship.getDashboard(y, x-1).isPresent() && !Component.areConnectorsCompatible(ship.getDashboard(y, x-1).get().connectors[1], connectors[3]) || // Left connector check
+            ship.getDashboard(y-1, x).isPresent() && !Component.areConnectorsCompatible(ship.getDashboard(y-1, x).get().connectors[2], connectors[0]) || // Top connector check
+            ship.getDashboard(y, x+1).isPresent() && !Component.areConnectorsCompatible(ship.getDashboard(y, x+1).get().connectors[3], connectors[1]) || // Right connector check
+            ship.getDashboard(y+1, x).isPresent() && !Component.areConnectorsCompatible(ship.getDashboard(y+1, x).get().connectors[0], connectors[2]) // Bottom connector check
+        ) throw new Exception();
     }
 
     public void rotateComponent(boolean clockwise) {
@@ -81,13 +94,69 @@ public class Component {
             newConnectors[1] = connectors[0];
             newConnectors[2] = connectors[1];
             newConnectors[3] = connectors[2];
-        } else {
+        }
+        else {
             newConnectors[0] = connectors[1];
             newConnectors[1] = connectors[2];
             newConnectors[2] = connectors[3];
             newConnectors[3] = connectors[0];
         }
         connectors = newConnectors;
+    }
+
+    public void destroyComponent(Ship ship) {
+        affectDestroy(ship);
+
+        // Matrix of booleans to track visited components
+        boolean[][] visited = new boolean[ship.getDashboard().length][ship.getDashboard()[0].length];
+        List<List<Component>> groups = new ArrayList<>();
+
+        // Find connected groups
+        for (int i = 0; i < ship.getDashboard().length; i++) {
+            for (int j = 0; j < ship.getDashboard()[0].length; j++) {
+                if (ship.getDashboard(i, j).isPresent() && !visited[i][j]) {
+                    List<Component> group = new ArrayList<>();
+                    dfs(ship.getDashboard(), i, j, visited, group, Optional.empty());
+                    groups.add(group);
+                }
+            }
+        }
+
+        if (groups.size() > 1) { // TODO (or have no more cabins)
+            int toKeep = 0; // View
+            for (int i = 0; i < groups.size(); i++) {
+                if (i != toKeep) {
+                    for (Component componentToRemove : groups.get(i)) {
+                        componentToRemove.affectDestroy(ship);
+                    }
+                }
+            }
+        }
+    }
+
+    private void dfs(Optional<Component>[][] dashboard, int i, int j, boolean[][] visited, List<Component> group, Optional<Component> otherComponentOpt) {
+        if (i < 0 || i >= dashboard.length || j < 0 || j >= dashboard[0].length) return;
+        if (visited[i][j] || dashboard[i][j].isEmpty()) return;
+
+        // Skip if connectors are not linked together
+        if (otherComponentOpt.isPresent()) {
+            if (
+                dashboard[i][j].get().getX() < otherComponentOpt.get().getX() && !Component.areConnectorsLinked(dashboard[i][j].get().getConnectors()[1], otherComponentOpt.get().getConnectors()[3]) ||
+                dashboard[i][j].get().getX() > otherComponentOpt.get().getX() && !Component.areConnectorsLinked(dashboard[i][j].get().getConnectors()[3], otherComponentOpt.get().getConnectors()[1]) ||
+                dashboard[i][j].get().getY() < otherComponentOpt.get().getY() && !Component.areConnectorsLinked(dashboard[i][j].get().getConnectors()[0], otherComponentOpt.get().getConnectors()[2]) ||
+                dashboard[i][j].get().getY() > otherComponentOpt.get().getY() && !Component.areConnectorsLinked(dashboard[i][j].get().getConnectors()[2], otherComponentOpt.get().getConnectors()[0])
+            )
+                return;
+        }
+
+        visited[i][j] = true;
+        dashboard[i][j].ifPresent(group::add);
+
+        // Check close components
+        dfs(dashboard, i - 1, j, visited, group, dashboard[i][j]);
+        dfs(dashboard, i + 1, j, visited, group, dashboard[i][j]);
+        dfs(dashboard, i, j - 1, visited, group, dashboard[i][j]);
+        dfs(dashboard, i, j + 1, visited, group, dashboard[i][j]);
     }
 
     public int getX() {
@@ -97,4 +166,13 @@ public class Component {
     public int getY() {
         return y;
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }
