aabfe7425cd1ef4c6bb8f8fa5b78b28effc8d4c2
Davide Cutrupi <davidecutrupi2@gmail.com>
2025-03-10 12:55:29 +0100

    Add combact zone card logic

--- src/main/java/it/polimi/ingsw/model/cards/utils/PenaltyCombatZone.java
+++ src/main/java/it/polimi/ingsw/model/cards/utils/PenaltyCombatZone.java
@@ -1,6 +1,12 @@
 package it.polimi.ingsw.model.cards.utils;
 
+import it.polimi.ingsw.model.game.Board;
+import it.polimi.ingsw.model.player.PlayerData;
+
 public abstract class PenaltyCombatZone {
-    public PenaltyCombatZone() {
-    }
+
+    public PenaltyCombatZone() {}
+
+    public abstract void resolve(Board board, PlayerData player) throws Exception;
+
 }
