3f9d6f0d961c0f15f1e1059631a28b4093ad111b
Davide Cutrupi <davidecutrupi2@gmail.com>
2025-03-10 12:54:29 +0100

    Add hit methods in meteor and cannon fire

--- src/main/java/it/polimi/ingsw/model/cards/utils/Meteor.java
+++ src/main/java/it/polimi/ingsw/model/cards/utils/Meteor.java
@@ -1,21 +1,44 @@
 package it.polimi.ingsw.model.cards.utils;
 
+import it.polimi.ingsw.model.components.BatteryComponent;
+import it.polimi.ingsw.model.components.Component;
+import it.polimi.ingsw.model.components.utils.ConnectorType;
+import it.polimi.ingsw.model.player.Ship;
 import it.polimi.ingsw.model.properties.DirectionType;
+
+import java.util.Optional;
 
 public class Meteor {
     private final boolean isBig;
-    private final DirectionType directionForm;
+    private final DirectionType directionFrom;
 
-    public Meteor(boolean isBig, DirectionType directionForm) {
+    public Meteor(boolean isBig, DirectionType directionFrom) {
         this.isBig = isBig;
-        this.directionForm = directionForm;
+        this.directionFrom = directionFrom;
     }
 
-    public boolean getIsBig() {
-        return isBig;
+    public void hit(Ship ship, int coord) throws Exception {
+        if (coord > 10 || coord < 4) return; // Miss
+
+        Optional<Component> targetOpt = directionFrom.firstComponentFromThisDirection(ship.getDashboard(), coord); // Find hit component
+        if (targetOpt.isEmpty()) return; // Miss
+        Component target = targetOpt.get();
+
+        if (!isBig && target.getConnectors()[directionFrom.ordinal()] == ConnectorType.EMPTY) // Ship is safe
+            return;
+        else if (!isBig && ship.getProtectedSides().contains(directionFrom) && ship.getBatteries() > 0) { // Ask user if he wants to use a battery
+            Optional<BatteryComponent> chosenComponent = Optional.empty(); // View
+            if (chosenComponent.isPresent()) { // Ship is safe
+                chosenComponent.get().useBattery(ship);
+                return;
+            }
+        }
+        else if (isBig) {
+            // TODO
+            // Ask user if he wants to activate cannon
+        }
+
+        target.destroyComponent(ship); // Destroy component
     }
 
-    public DirectionType getDirectionForm() {
-        return directionForm;
-    }
 }
