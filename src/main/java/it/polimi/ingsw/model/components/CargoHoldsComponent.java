79724f14911346cb2500c4fd74101a51a3137072
Davide Cutrupi <davidecutrupi2@gmail.com>
2025-03-10 12:51:33 +0100

    Add load and unload constraints (no red goods)

--- src/main/java/it/polimi/ingsw/model/components/CargoHoldsComponent.java
+++ src/main/java/it/polimi/ingsw/model/components/CargoHoldsComponent.java
@@ -1,11 +1,25 @@
 package it.polimi.ingsw.model.components;
 
 import it.polimi.ingsw.model.components.utils.ConnectorType;
+import it.polimi.ingsw.model.game.objects.ColorType;
+import it.polimi.ingsw.model.player.Ship;
 
 public class CargoHoldsComponent extends SpecialCargoHoldsComponent {
 
     public CargoHoldsComponent(ConnectorType[] connectors, int number) {
         super(connectors, number);
+    }
+
+    @Override
+    public void loadGood(ColorType good, Ship ship) throws Exception {
+        if (good == ColorType.RED) throw new Exception();
+        super.loadGood(good, ship);
+    }
+
+    @Override
+    public void unloadGood(ColorType good, Ship ship) throws Exception {
+        if (good == ColorType.RED) throw new Exception();
+        super.loadGood(good, ship);
     }
 
 }
