c90c59a3d1d38df390b0177661d50ecbb6baa895
david <davide.ditanna@gmail.com>
2025-03-10 18:01:40 +0100

    Epidemic Card Implementation and Testing

--- src/test/java/it/polimi/ingsw/model/cards/EpidemicCardTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/EpidemicCardTest.java
@@ -20,8 +20,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class EpidemicCardTest {
 
     private Optional<Component>[][] dashboard1;
-    private List<Optional<Component>> discarded;
-    private Optional<Component> component;
+    private List<Component> discarded;
+    private Component[] reserves;
     private int battery;
     private Map<ColorType, Integer> goods;
     private List<DirectionType> directions;
@@ -54,20 +54,18 @@ class EpidemicCardTest {
     }
 
     @Test
-    void testShouldReduceAliensInTwoAdjacentCabins() {
+    void testShouldReduceAliensInTwoAdjacentCabins() throws Exception {
         //setto i due alieni in due cabine adiacenti (setto manualmente gli umani a 0 per definizione)
-        cabin1.setAlien(AlienType.CANNON);
-        cabin2.setAlien(AlienType.ENGINE);
-        cabin1.setHumans(0);
-        cabin2.setHumans(0);
-        dashboard1[1][1] = Optional.of(cabin1);
-        cabin1.setX(1);
-        cabin1.setY(1);
-        dashboard1[1][2] = Optional.of(cabin2);
-        cabin2.setX(1);
-        cabin2.setY(2);
         // setto manualmente l'equipaggio iniziale a 4 (2 per ogni alieno)
-        Ship ship1 = new Ship(dashboard1, discarded, component, 4, battery, goods, directions);
+        Ship ship1 = new Ship(dashboard1, discarded, reserves);
+        cabin1.insertComponent(ship1, 1, 1);
+        cabin1.setHumans(0, ship1);
+        cabin1.setAlien(AlienType.CANNON, ship1);
+
+        cabin2.insertComponent(ship1, 1, 2);
+        cabin2.setHumans(0, ship1);
+        cabin2.setAlien(AlienType.ENGINE, ship1);
+
         PlayerData player1 = new PlayerData(ColorType.BLUE, "Simone", ship1, 0);
         players.add(new AbstractMap.SimpleEntry<>(player1, 2));
         EpidemicCard epidemicCard = new EpidemicCard(2, false);
@@ -76,26 +74,20 @@ class EpidemicCardTest {
         //verifico che sia stato aggiornato correttamente il valore dell'equipaggio
         assertEquals(0, player1.getShip().getCrew());
         //verifico che l'equipaggio sia stato rimosso nella plancia della nave
-        assertFalse(player1.getShip().getCabines().getFirst().getAlien().isPresent());
-        assertEquals(1, player1.getShip().getCabines().getFirst().getX());
+        assertFalse(player1.getShip().getComponentByType(CabinComponent.class).getFirst().getAlien().isPresent());
+        assertEquals(1, player1.getShip().getComponentByType(CabinComponent.class).getFirst().getX());
         assertFalse(((CabinComponent)player1.getShip().getDashboard()[1][1].get()).getAlien().isPresent());
 
     }
 
 
     @Test
-    void testShouldReduceHumansInThreeAdjacentCabins() {
-        cabin2.setHumans(1);
-        dashboard1[1][1] = Optional.of(cabin1);
-        cabin1.setX(1);
-        cabin1.setY(1);
-        dashboard1[1][2] = Optional.of(cabin2);
-        cabin2.setX(1);
-        cabin2.setY(2);
-        dashboard1[2][2] = Optional.of(cabin3);
-        cabin3.setX(2);
-        cabin3.setY(2);
-        Ship ship1 = new Ship(dashboard1, discarded, component, 5, battery, goods, directions);
+    void testShouldReduceHumansInThreeAdjacentCabins() throws Exception{
+        Ship ship1 = new Ship(dashboard1, discarded, reserves);
+        cabin1.insertComponent(ship1, 1, 1);
+        cabin2.insertComponent(ship1, 1, 2);
+        cabin2.setHumans(1, ship1);
+        cabin3.insertComponent(ship1, 2, 2);
         PlayerData player1 = new PlayerData(ColorType.BLUE, "Simone", ship1, 0);
         players.add(new AbstractMap.SimpleEntry<>(player1, 2));
         EpidemicCard epidemicCard = new EpidemicCard(2, false);
@@ -104,56 +96,47 @@ class EpidemicCardTest {
         //verifico che sia stato aggiornato correttamente il valore dell'equipaggio
         assertEquals(2, player1.getShip().getCrew());
 
-        assertEquals(1, player1.getShip().getCabines().getFirst().getHumans());
+        assertEquals(1, player1.getShip().getComponentByType(CabinComponent.class).getFirst().getHumans());
         assertEquals(1, ((CabinComponent)player1.getShip().getDashboard()[1][1].get()).getHumans());
         assertEquals(0, ((CabinComponent)player1.getShip().getDashboard()[1][2].get()).getHumans());
 
     }
 
     @Test
-    void testShouldNotReduceHumansInNonAdjacentCabins() {
-        CabinComponent cabin1 = new CabinComponent(connectors, false);
-        CabinComponent cabin3 = new CabinComponent(connectors, false);
-        dashboard1[1][1] = Optional.of(cabin1);
-        cabin1.setX(1);
-        cabin1.setY(1);
-        dashboard1[2][2] = Optional.of(cabin3);
-        cabin3.setX(2);
-        cabin3.setY(2);
-        Ship ship1 = new Ship(dashboard1, discarded, component, 4, battery, goods, directions);
+    void testShouldNotReduceHumansInNonAdjacentCabins() throws Exception {
+        Ship ship1 = new Ship(dashboard1, discarded, reserves);
+        cabin1.insertComponent(ship1, 1, 1);
+        cabin3.insertComponent(ship1, 2, 2);
+
         PlayerData player1 = new PlayerData(ColorType.BLUE, "Simone", ship1, 0);
         players.add(new AbstractMap.SimpleEntry<>(player1, 2));
         EpidemicCard epidemicCard = new EpidemicCard(2, false);
         epidemicCard.resolve(board);
 
         assertEquals(4, player1.getShip().getCrew());
-        assertEquals(2, player1.getShip().getCabines().getFirst().getHumans());
-        assertEquals(1, player1.getShip().getCabines().getFirst().getX());
+        assertEquals(2, player1.getShip().getComponentByType(CabinComponent.class).getFirst().getHumans());
+        assertEquals(1, player1.getShip().getComponentByType(CabinComponent.class).getFirst().getX());
         assertEquals(2, ((CabinComponent)player1.getShip().getDashboard()[1][1].get()).getHumans());
 
     }
 
     @Test
-    void testShouldNotReduceHumanAndAlienInNonAdjacentCabins() {
-        cabin1.setAlien(AlienType.CANNON);
-        cabin1.setHumans(0);
-        cabin2.setHumans(1);
-        dashboard1[1][1] = Optional.of(cabin1);
-        cabin1.setX(1);
-        cabin1.setY(1);
-        dashboard1[2][2] = Optional.of(cabin2);
-        cabin2.setX(2);
-        cabin2.setY(2);
-        Ship ship1 = new Ship(dashboard1, discarded, component, 3, battery, goods, directions);
+    void testShouldNotReduceHumanAndAlienInNonAdjacentCabins() throws Exception {
+        Ship ship1 = new Ship(dashboard1, discarded, reserves);
+        cabin1.insertComponent(ship1, 1, 1);
+        cabin1.setAlien(AlienType.CANNON, ship1);
+        cabin2.insertComponent(ship1, 2, 2);
+        cabin2.setHumans(1, ship1);
+
         PlayerData player1 = new PlayerData(ColorType.BLUE, "Simone", ship1, 0);
         players.add(new AbstractMap.SimpleEntry<>(player1, 2));
         EpidemicCard epidemicCard = new EpidemicCard(2, false);
         epidemicCard.resolve(board);
 
         assertEquals(3, player1.getShip().getCrew());
-        assertEquals(0, player1.getShip().getCabines().getFirst().getHumans());
+        assertEquals(0, player1.getShip().getComponentByType(CabinComponent.class).getFirst().getHumans());
         assertEquals(0, ((CabinComponent)player1.getShip().getDashboard()[1][1].get()).getHumans());
-        assertTrue(player1.getShip().getCabines().getFirst().getAlien().isPresent());
+        assertTrue(player1.getShip().getComponentByType(CabinComponent.class).getFirst().getAlien().isPresent());
         assertTrue(((CabinComponent)player1.getShip().getDashboard()[1][1].get()).getAlien().isPresent());
 
     }
