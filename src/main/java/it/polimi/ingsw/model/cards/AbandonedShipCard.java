976379a78ff88c06e94c1b7112d92d427f6ff362
david <davide.ditanna@gmail.com>
2025-03-24 16:36:15 +0100

    Implementation of AbandonedShip Card

--- src/main/java/it/polimi/ingsw/model/cards/AbandonedShipCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/AbandonedShipCard.java
@@ -1,17 +1,18 @@
 package it.polimi.ingsw.model.cards;
 
-import it.polimi.ingsw.model.components.CabinComponent;
 import it.polimi.ingsw.model.game.Board;
 import it.polimi.ingsw.model.player.PlayerData;
 
-import java.util.AbstractMap;
 import java.util.List;
-import java.util.Optional;
 
 public class AbandonedShipCard extends Card {
     private final int crew;
     private final int credits;
     private final int days;
+    private int playerIndex;
+    private int conquerorPlayerIndex;
+    private List<PlayerData> players;
+    private boolean shipConquered;
 
     public AbandonedShipCard(int level, boolean isLearner, int crew, int credits, int days) {
         super(level, isLearner);
@@ -20,49 +21,78 @@ public class AbandonedShipCard extends Card {
         this.days = days;
     }
 
-    public int getCrew() {
-        return crew;
+    public void startCard(Board board) {
+        this.playerIndex = 0;
+        this.conquerorPlayerIndex = -1;
+        this.players = board.getPlayersByPos();
+        this.shipConquered = false;
+
+        for (PlayerData player : board.getPlayersByPos()) {
+            playersState.put(player.getUsername(), CardState.WAIT);
+        }
+        autoCheckPlayers();
     }
 
-    public int getCredits() {
-        return credits;
-    }
+    public void changeState(Board board, String username) {
 
-    public int getDays() {
-        return days;
-    }
+        CardState actState = playersState.get(username);
 
-    @Override
-    public void resolve(Board board) throws Exception {
-        List<AbstractMap.SimpleEntry<PlayerData, Integer>> players = board.getPlayers();
-
-        // iterate on the list of player -> for now I've not implemented if a player does not want to
-        // play this card, it's only to iterate on the list of player
-        for (AbstractMap.SimpleEntry<PlayerData, Integer> entry : players) {
-            PlayerData player = entry.getKey();
-
-            int cardCrew = getCrew();
-            int crew = player.getShip().getCrew();
-            int credits = player.getCredits();
-
-            // check if the player can really play the card
-            if (crew > cardCrew) {
-                // player need to choose if he wants to get rid of alien and or human
-                while (cardCrew > 0) {
-                    Optional<CabinComponent> chosenComponentOpt = Optional.empty(); // View
-                    CabinComponent chosenComponent = chosenComponentOpt.orElseThrow();
-                    if (chosenComponent.getAlien().isEmpty() && chosenComponent.getHumans() > 0) {
-                        chosenComponent.setHumans(chosenComponent.getHumans() - 1, player.getShip());
-                        cardCrew --;
-                    }
-                    else if (chosenComponent.getAlien().isPresent() && chosenComponent.getHumans() == 0) {
-                        chosenComponent.setAlien(null, player.getShip());
-                        cardCrew =- 2;
-                    }
-
+        switch (actState) {
+            case WAIT_CREW -> playersState.put(username, CardState.DONE);
+            case WAIT_BOOLEAN -> {
+                if (conquerorPlayerIndex == players.indexOf(getPlayerbyEntiy(username))) { // basta !=-1 ??
+                    playersState.put(username, CardState.WAIT_CREW);
+                    shipConquered = true;
                 }
-                board.movePlayer(player, getDays() * -1);
+                else
+                    playersState.put(username, CardState.DONE);
+
             }
         }
+
+        playerIndex++;
+        autoCheckPlayers();
+
     }
+
+    private void autoCheckPlayers() {
+        for (; playerIndex < players.size(); playerIndex++) {
+            PlayerData player = players.get(playerIndex);
+
+            if (shipConquered)
+                playersState.put(player.getUsername(), CardState.DONE);
+            else if (player.getShip().getCrew() < crew) // User loses automatically
+                playersState.put(player.getUsername(), CardState.DONE);
+            else { // User could win
+                playersState.put(player.getUsername(), CardState.WAIT_BOOLEAN);
+                return;
+            }
+        }
+
+        // Check if everyone has finished
+        boolean hasDone = true;
+        for (PlayerData player : players)
+            if (playersState.get(player.getUsername()) != CardState.DONE)
+                hasDone = false;
+
+        if (hasDone) {
+            endCard();
+        }
+    }
+
+    public void endCard() {
+
+    }
+
+    public void doCommandEffects(CardState commandType, Boolean value, String username, Board board) {
+        PlayerData player = board.getPlayerEntitybyUsername(username);
+        if (commandType == CardState.WAIT_BOOLEAN && value) {
+            conquerorPlayerIndex = players.indexOf(getPlayerbyEntity(username));
+        }
+        else if (commandType == CardState.WAIT_CREW) {
+            board.movePlayer(player, days * -1);
+            player.setCredits(credits + player.getCredits());
+        }
+    }
+
 }
