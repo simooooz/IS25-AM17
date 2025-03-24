186a438a5bd580c8abc205354bd58d02ddc70686
david <davide.ditanna@gmail.com>
2025-03-24 16:35:57 +0100

    Implementation of Planet Card

--- src/main/java/it/polimi/ingsw/model/cards/PlanetCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/PlanetCard.java
@@ -1,68 +1,81 @@
 package it.polimi.ingsw.model.cards;
 
 import it.polimi.ingsw.model.cards.utils.Planet;
-import it.polimi.ingsw.model.game.objects.ColorType;
 import it.polimi.ingsw.model.game.Board;
 import it.polimi.ingsw.model.player.PlayerData;
 
-import java.util.AbstractMap.SimpleEntry;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public class PlanetCard extends Card{
 
-    private final Map<Planet, Optional<PlayerData>> planets;
+    private final List<Planet> planets
+    private Map<PlayerData, Planet> landedPlayers;
     private final int days;
+    private int playerIndex;
 
-    public PlanetCard(int level, boolean isLearner, Map<Planet, Optional<PlayerData>> planets, int days) {
+
+    public PlanetCard(int level, boolean isLearner, List<Planet> planets, int days) {
         super(level, isLearner);
         this.planets = planets;
         this.days = days;
     }
 
-    public Map<Planet, Optional<PlayerData>> getPlanets() {
-        return planets;
+
+    public void startCard(Board board){
+        this.playerIndex = 0;
+        this.landedPlayers = new HashMap<>();
+        for (PlayerData player: board.getPlayersByPos()){
+            playersState.put(player.getUsername(), CardState.WAIT);
+        }
+        playersState.put(board.getPlayersByPos().getFirst().getUsername(), CardState.WAIT_INDEX);
     }
 
-    public int getDays() {
-        return days;
-    }
+    public void changeState(Board board, String username) throws Exception {
 
-    @Override
-    public void resolve(Board board){
-        for (SimpleEntry<PlayerData, Integer> entry : board.getPlayers()) {
-            PlayerData player = entry.getKey();
-            Optional<Planet> landingPlanet=null;
-            //Optional.of(pianetadaricevere);
-            //chiedo l'eventuale pianeta all'utente
-            landingPlanet.ifPresent((Planet planet) -> {
-                planet.getRewards().keySet().forEach(goodType -> {
-                    int rewardTypeG = planet.getRewards().get(goodType);
-                    int availableG = board.getAvailableGoods().get(goodType);
-                    int playerG = player.getShip().getGoods().get(goodType);
-                    int toDecrease = Math.min(availableG, rewardTypeG);
-                    while (toDecrease > 0) {
-                        //chiedo alla view il componente sul quale inserire la merce
-                        // CargoComponent c1 = Funzionecherichiedeilcomponenteallview()
-                        // c1.loadGood(goodType);
-                        toDecrease--;
-                    }
-                    board.getAvailableGoods().put(goodType, availableG - toDecrease);
-                    player.getShip().getGoods().put(goodType, playerG + toDecrease);
-                });
-            });
-        };
+        CardState actState = playersState.get(username);
 
-        //dopo che tutti hanno scelto se atterrare decremento i giorni di volo in ordine inverso di rotta
-        for(int i=board.getPlayers().size()-1;i>=0;i--){
-            PlayerData player = board.getPlayers().get(i).getKey();
-            if(planets.values().stream()
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .anyMatch(playerData -> playerData.equals(player))){
-                        board.movePlayer(player, getDays()*-1);
+        switch (actState){
+            case WAIT_GOODS -> playersState.put(username, CardState.DONE);
+            case WAIT_INDEX -> {
+                if (landedPlayers.containsKey(board.getPlayerEntitybyUsername(username))) {
+                    playersState.put(username, CardState.WAIT_GOODS);
+                }
+                else {
+                    playersState.put(username, CardState.DONE);
+                }
             }
         }
+
+        playerIndex++;
+        if (playerIndex < board.getPlayersByPos().size())
+            playersState.put(board.getPlayersByPos().get(playerIndex).getUsername(), CardState.WAIT_INDEX);
+
+        // Check if everyone has finished
+        boolean hasDone = true;
+        for (PlayerData player : board.getPlayersByPos())
+            if (playersState.get(player.getUsername()) != CardState.DONE)
+                hasDone = false;
+
+        if (hasDone) {
+            for (PlayerData player : board.getPlayersByPos().reversed())
+                if (landedPlayers.containsKey(player))
+                    board.movePlayer(player, days * -1);
+            endCard();
+        }
     }
+
+    public void endCard() {
+
+    }
+
+    public void doCommandEffects(CardState commandType, Integer value, String username, Board board) {
+        PlayerData player = board.getPlayerEntitybyUsername(username);
+        // if player decides to land on a planet
+        if (commandType == CardState.WAIT_INDEX && value != -1)
+            landedPlayers.put(player, planets.get(value));
+    }
+
 
 }
