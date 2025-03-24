50b7014dbfed0afac979ff0731d8320463d3aa80
david <davide.ditanna@gmail.com>
2025-03-24 16:35:48 +0100

    New Implementation of Epidemic Card

--- src/main/java/it/polimi/ingsw/model/cards/EpidemicCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/EpidemicCard.java
@@ -4,54 +4,51 @@ import it.polimi.ingsw.model.components.CabinComponent;
 import it.polimi.ingsw.model.player.PlayerData;
 import it.polimi.ingsw.model.game.Board;
 
-import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
-import java.util.Optional;
 
-import it.polimi.ingsw.model.game.Board;
 
-public class EpidemicCard extends Card{
+public class EpidemicCard extends Card {
+
     public EpidemicCard(int level, boolean isLearner) {
         super(level, isLearner);
     }
 
-
-// metodo che decrementa i membri di una cabina (sia nel caso umani che nel caso alieni)
-    private void uploadCabin(List<CabinComponent> cabines, int i, PlayerData playerData) throws Exception {
-        if (cabines.get(i).getHumans() > 0) {
-            cabines.get(i).setHumans(cabines.get(i).getHumans() - 1, playerData.getShip());
-        }
-        else if (cabines.get(i).getAlien().isPresent()) {
-            cabines.get(i).setAlien(null, playerData.getShip());
-
-        }
-    }
-
-    @Override
-    public void resolve(Board board) throws Exception {
-        //per ogni giocatore
-        for (SimpleEntry<PlayerData, Integer> entry : board.getPlayers()) {
-            PlayerData player = entry.getKey();
-            // ottengo tutte le sue cabine
+    public void startCard(Board board) throws Exception {
+        for (PlayerData player : board.getPlayersByPos()) {
             List<CabinComponent> cabins = player.getShip().getComponentByType(CabinComponent.class);
-            boolean[] checkEpidemic = new boolean[cabins.size()]; //inizializzato tutto a false di default
+            boolean[] checkEpidemic = new boolean[cabins.size()]; // to check if cabins are already visited
+
             for (int i = 0; i < cabins.size(); i++) {
                 for (int j = i+1; j < cabins.size(); j++) {
-                    //se due cabine sono vicine
+
                     if (cabins.get(i).isNearTo(cabins.get(j))) {
-                        // se non le ho già visitate, riimuovo l'equipaggio e segno che le ho visitate
-                        if (!checkEpidemic[i]) {
-                            uploadCabin(cabins, i, player);
+                        if (!checkEpidemic[i]) { // if not visited yet
+                            decrementCrew(cabins, i, player);
                             checkEpidemic[i] = true;
-                        };
-                        if (!checkEpidemic[j]) {
-                            uploadCabin(cabins, j, player);
+                        }
+                        if (!checkEpidemic[j]) { // if not visited yet
+                            decrementCrew(cabins, j, player);
                             checkEpidemic[j] = true;
                         }
                     }
+
                 }
             }
         }
+
+        endCard();
+    }
+
+
+    private void decrementCrew(List<CabinComponent> cabines, int i, PlayerData playerData) throws Exception {
+        if (cabines.get(i).getHumans() > 0)
+            cabines.get(i).setHumans(cabines.get(i).getHumans() - 1, playerData.getShip());
+        else if (cabines.get(i).getAlien().isPresent())
+            cabines.get(i).setAlien(null, playerData.getShip());
+    }
+
+    public void endCard() {
+
     }
 
 }
