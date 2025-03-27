b762e9055660917d6302702b554e740c166e7f6f
david <davide.ditanna@gmail.com>
2025-03-27 22:29:39 +0100

    RollDicesCommand Implementation

--- src/main/java/it/polimi/ingsw/model/cards/commands/RollDicesCommand.java
+++ src/main/java/it/polimi/ingsw/model/cards/commands/RollDicesCommand.java
@@ -1,12 +1,24 @@
 package it.polimi.ingsw.model.cards.commands;
 
+import it.polimi.ingsw.model.cards.Card;
+import it.polimi.ingsw.model.cards.PlayerState;
+import it.polimi.ingsw.model.game.Board;
 import it.polimi.ingsw.model.game.objects.Dice;
 
-public class RollDicesCommand implements Command<Integer> {
+public class RollDicesCommand implements Command {
+
+    private final String username;
+    private final Board board;
+
+    public RollDicesCommand(String username, Board board) {
+        this.username = username;
+        this.board = board;
+    }
 
     @Override
-    public Integer execute() {
-        return Dice.roll() + Dice.roll();
+    public void execute(Card card) {
+        int res = Dice.roll() + Dice.roll();
+        card.doCommandEffects(PlayerState.WAIT_ROLL_DICES, res, username, board);
     }
 
 }
