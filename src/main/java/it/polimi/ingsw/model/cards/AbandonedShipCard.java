aed50b66decec4efbb968995d34447575c12de8a
Tommaso D’Alessio <tommydale03@gmail.com>
2025-03-27 21:36:29 +0100

    update of AbandonedShipCard

--- src/main/java/it/polimi/ingsw/model/cards/AbandonedShipCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/AbandonedShipCard.java
@@ -1,14 +1,17 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.model.components.CabinComponent;
 import it.polimi.ingsw.model.game.Board;
 import it.polimi.ingsw.model.player.PlayerData;
 
 import java.util.List;
 
 public class AbandonedShipCard extends Card {
+
     private final int crew;
     private final int credits;
     private final int days;
+
     private int playerIndex;
     private int conquerorPlayerIndex;
     private List<PlayerData> players;
@@ -21,78 +24,83 @@ public class AbandonedShipCard extends Card {
         this.days = days;
     }
 
-    public void startCard(Board board) {
+    @Override
+    public boolean startCard(Board board) {
         this.playerIndex = 0;
         this.conquerorPlayerIndex = -1;
         this.players = board.getPlayersByPos();
         this.shipConquered = false;
 
-        for (PlayerData player : board.getPlayersByPos()) {
-            playersState.put(player.getUsername(), CardState.WAIT);
-        }
-        autoCheckPlayers();
+        for (PlayerData player : board.getPlayersByPos())
+            playersState.put(player.getUsername(), PlayerState.WAIT);
+
+        return autoCheckPlayers(board);
     }
 
-    public void changeState(Board board, String username) {
+    @Override
+    protected boolean changeState(Board board, String username) {
 
-        CardState actState = playersState.get(username);
+        PlayerState actState = playersState.get(username);
 
         switch (actState) {
-            case WAIT_CREW -> playersState.put(username, CardState.DONE);
+            case WAIT_REMOVE_CREW -> {
+                playersState.put(username, PlayerState.DONE);
+                PlayerData player = board.getPlayerEntityByUsername(username);
+                board.movePlayer(player, days * -1);
+                player.setCredits(credits + player.getCredits());
+            }
             case WAIT_BOOLEAN -> {
-                if (conquerorPlayerIndex == players.indexOf(getPlayerbyEntiy(username))) { // basta !=-1 ??
-                    playersState.put(username, CardState.WAIT_CREW);
+                if (conquerorPlayerIndex == players.indexOf(board.getPlayerEntityByUsername(username))) {
+                    playersState.put(username, PlayerState.WAIT_REMOVE_CREW);
                     shipConquered = true;
                 }
                 else
-                    playersState.put(username, CardState.DONE);
-
+                    playersState.put(username, PlayerState.DONE);
             }
         }
 
         playerIndex++;
-        autoCheckPlayers();
-
+        return autoCheckPlayers(board);
     }
 
-    private void autoCheckPlayers() {
+    private boolean autoCheckPlayers(Board board) {
         for (; playerIndex < players.size(); playerIndex++) {
             PlayerData player = players.get(playerIndex);
 
             if (shipConquered)
-                playersState.put(player.getUsername(), CardState.DONE);
+                playersState.put(player.getUsername(), PlayerState.DONE);
             else if (player.getShip().getCrew() < crew) // User loses automatically
-                playersState.put(player.getUsername(), CardState.DONE);
+                playersState.put(player.getUsername(), PlayerState.DONE);
             else { // User could win
-                playersState.put(player.getUsername(), CardState.WAIT_BOOLEAN);
-                return;
+                playersState.put(player.getUsername(), PlayerState.WAIT_BOOLEAN);
+                return false;
             }
         }
 
         // Check if everyone has finished
         boolean hasDone = true;
         for (PlayerData player : players)
-            if (playersState.get(player.getUsername()) != CardState.DONE)
+            if (playersState.get(player.getUsername()) != PlayerState.DONE)
                 hasDone = false;
 
         if (hasDone) {
-            endCard();
+            endCard(board);
+            return true;
         }
+
+        return false;
     }
 
-    public void endCard() {
-
+    @Override
+    public void doCommandEffects(PlayerState commandType, Boolean value, String username, Board board) {
+        PlayerData player = board.getPlayerEntityByUsername(username);
+        if (commandType == PlayerState.WAIT_BOOLEAN && value)
+            conquerorPlayerIndex = players.indexOf(player);
     }
 
-    public void doCommandEffects(CardState commandType, Boolean value, String username, Board board) {
-        PlayerData player = board.getPlayerEntitybyUsername(username);
-        if (commandType == CardState.WAIT_BOOLEAN && value) {
-            conquerorPlayerIndex = players.indexOf(getPlayerbyEntity(username));
-        }
-        else if (commandType == CardState.WAIT_CREW) {
-            board.movePlayer(player, days * -1);
-            player.setCredits(credits + player.getCredits());
-        }
+    @Override
+    public void doSpecificCheck(PlayerState commandType, List<CabinComponent> cabins, int toRemove, String username, Board board) {
+        super.doSpecificCheck(commandType, cabins, this.crew, username, board);
     }
 
 }
