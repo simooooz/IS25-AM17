53719be10ee630ef32ce2fb63f41e3fe010f8903
david <davide.ditanna@gmail.com>
2025-03-27 22:29:32 +0100

    RemoveCrewCommand Implementation

--- src/main/java/it/polimi/ingsw/model/cards/commands/RemoveCrewCommand.java
+++ src/main/java/it/polimi/ingsw/model/cards/commands/RemoveCrewCommand.java
@@ -1,32 +1,60 @@
 package it.polimi.ingsw.model.cards.commands;
 
+import it.polimi.ingsw.model.cards.Card;
+import it.polimi.ingsw.model.cards.PlayerState;
 import it.polimi.ingsw.model.components.CabinComponent;
+import it.polimi.ingsw.model.components.Component;
+import it.polimi.ingsw.model.exceptions.BatteryComponentNotValidException;
+import it.polimi.ingsw.model.exceptions.CabinComponentNotValidException;
+import it.polimi.ingsw.model.exceptions.ComponentNotValidException;
+import it.polimi.ingsw.model.game.Board;
 import it.polimi.ingsw.model.player.Ship;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class RemoveCrewCommand implements Command<Void> {
+public class RemoveCrewCommand implements Command {
 
-    private final Ship ship;
+    private final String username;
+    private final Board board;
     private final List<CabinComponent> cabinComponents;
 
-    public RemoveCrewCommand(Ship ship, List<CabinComponent> cabinComponents) {
-        this.ship = ship;
+    public RemoveCrewCommand(String username, Board board, List<CabinComponent> cabinComponents) {
+        this.username = username;
+        this.board = board;
         this.cabinComponents = cabinComponents;
     }
 
     @Override
-    public Void execute() {
+    public void execute(Card card) {
+        Ship ship = board.getPlayerEntityByUsername(username).getShip();
+        checkInput(ship);
+        card.doSpecificCheck(PlayerState.WAIT_REMOVE_CREW, cabinComponents, 0, username, board);
+
         for (CabinComponent cabin : cabinComponents) {
             if (cabin.getAlien().isPresent())
                 cabin.setAlien(null, ship);
             else
                 cabin.setHumans(cabin.getHumans() - 1, ship);
-
         }
-        return null;
+
+    }
+
+    private void checkInput(Ship ship) {
+        for (CabinComponent component : cabinComponents)
+            if (ship.getDashboard(component.getY(), component.getX()).isEmpty() || !ship.getDashboard(component.getY(), component.getX()).get().equals(component))
+                throw new ComponentNotValidException("Cabin component not valid");
+
+        boolean enoughCrew = cabinComponents.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .allMatch(entry -> entry.getValue() <= (entry.getKey().getAlien().isPresent() ? 2 : entry.getKey().getHumans()));
+        if (!enoughCrew)
+            throw new CabinComponentNotValidException("Not enough crew");
     }
 
 }
