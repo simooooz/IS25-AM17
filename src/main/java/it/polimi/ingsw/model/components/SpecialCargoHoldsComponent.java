f4b1f2ef7647360912e8c9efb4b5964f138e2911
Tommaso D’Alessio <tommydale03@gmail.com>
2025-03-27 09:20:47 +0100

    update of SpecialCargoHoldsComponent

--- src/main/java/it/polimi/ingsw/model/components/SpecialCargoHoldsComponent.java
+++ src/main/java/it/polimi/ingsw/model/components/SpecialCargoHoldsComponent.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model.components;
 
 import it.polimi.ingsw.model.components.utils.ConnectorType;
+import it.polimi.ingsw.model.exceptions.GoodNotValidException;
 import it.polimi.ingsw.model.game.objects.ColorType;
 import it.polimi.ingsw.model.player.Ship;
 
@@ -26,20 +27,20 @@ public class SpecialCargoHoldsComponent extends Component {
         return goods;
     }
 
-    public void loadGood(ColorType good, Ship ship) throws Exception {
-        if (number == goods.size()) throw new Exception();
+    public void loadGood(ColorType good, Ship ship) {
+        if (number == goods.size()) throw new GoodNotValidException("Cargo hold is full");
         goods.add(good);
         ship.getGoods().put(good, ship.getGoods().get(good) + 1);
     }
 
-    public void unloadGood(ColorType good, Ship ship) throws Exception {
-        if (goods.isEmpty() || !goods.contains(good)) throw new Exception();
+    public void unloadGood(ColorType good, Ship ship) {
+        if (goods.isEmpty() || !goods.contains(good)) throw new GoodNotValidException("Cargo hold is empty");
         goods.remove(good);
         ship.getGoods().put(good, ship.getGoods().get(good) - 1);
     }
 
     @Override
-    public void affectDestroy(Ship ship) throws Exception {
+    public void affectDestroy(Ship ship) {
         super.affectDestroy(ship);
         for (ColorType good : goods) {
             ship.getGoods().put(good, ship.getGoods().get(good) - 1);
