b6fa3e1c12564f1660be1904d84b804a4290ccd7
david <davide.ditanna@gmail.com>
2025-03-27 22:29:47 +0100

    ShieldCommand Implementation

--- src/main/java/it/polimi/ingsw/model/cards/commands/ShieldCommand.java
+++ src/main/java/it/polimi/ingsw/model/cards/commands/ShieldCommand.java
@@ -1,27 +1,49 @@
 package it.polimi.ingsw.model.cards.commands;
 
+import it.polimi.ingsw.model.cards.Card;
+import it.polimi.ingsw.model.cards.PlayerState;
 import it.polimi.ingsw.model.components.BatteryComponent;
+import it.polimi.ingsw.model.exceptions.BatteryComponentNotValidException;
+import it.polimi.ingsw.model.exceptions.ComponentNotValidException;
+import it.polimi.ingsw.model.game.Board;
 import it.polimi.ingsw.model.player.Ship;
 
 import java.util.Optional;
 
-public class ShieldCommand implements Command<Boolean> {
+public class ShieldCommand implements Command {
 
-    private final Ship ship;
+    private final String username;
+    private final Board board;
     private final Optional<BatteryComponent> battery;
 
-    public ShieldCommand(Ship ship, BatteryComponent battery) {
-        this.ship = ship;
+    public ShieldCommand(String username, Board board, BatteryComponent battery) {
+        this.username = username;
+        this.board = board;
         this.battery = Optional.ofNullable(battery);
     }
 
     @Override
-    public Boolean execute() {
+    public void execute(Card card) {
+        Ship ship = board.getPlayerEntityByUsername(username).getShip();
+        checkInput(ship);
+
         if (battery.isPresent()) {
             battery.get().useBattery(ship);
-            return true;
+            card.doCommandEffects(PlayerState.WAIT_SHIELD, true, username, board);
         }
-        return false;
+        else
+            card.doCommandEffects(PlayerState.WAIT_SHIELD, false, username, board);
+    }
+
+    private void checkInput(Ship ship) {
+        if (battery.isEmpty()) return;
+        BatteryComponent component = battery.get();
+
+        if (ship.getDashboard(component.getY(), component.getX()).isEmpty() || !ship.getDashboard(component.getY(), component.getX()).get().equals(component))
+            throw new ComponentNotValidException("Battery component not valid");
+
+        if (component.getBatteries() == 0)
+            throw new BatteryComponentNotValidException("Not enough batteries");
     }
 
 }
