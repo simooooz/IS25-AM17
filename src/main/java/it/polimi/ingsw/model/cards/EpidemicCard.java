52a7ac7505c36da80f95d48d0a9b0d5a1c025188
Davide Cutrupi <davidecutrupi2@gmail.com>
2025-04-01 16:14:13 +0200

    Fix edge test case

--- src/main/java/it/polimi/ingsw/model/cards/EpidemicCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/EpidemicCard.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.model.components.CabinComponent;
 import it.polimi.ingsw.model.player.PlayerData;
 import it.polimi.ingsw.model.game.Board;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -18,34 +19,40 @@ public class EpidemicCard extends Card {
         for (PlayerData player : board.getPlayersByPos()) {
             List<CabinComponent> cabins = player.getShip().getComponentByType(CabinComponent.class);
             boolean[] checkEpidemic = new boolean[cabins.size()]; // to check if cabins are already visited
+            List<CabinComponent> toDecrease = new ArrayList<>();
 
             for (int i = 0; i < cabins.size(); i++) {
                 for (int j = i+1; j < cabins.size(); j++) {
 
                     if (cabins.get(i).isNearTo(cabins.get(j)) && (cabins.get(i).getHumans() > 0 || cabins.get(i).getAlien().isPresent()) && (cabins.get(j).getHumans() > 0 || cabins.get(j).getAlien().isPresent())) {
                         if (!checkEpidemic[i]) { // if not visited yet
-                            decrementCrew(cabins, i, player);
+                            toDecrease.add(cabins.get(i));
                             checkEpidemic[i] = true;
                         }
                         if (!checkEpidemic[j]) { // if not visited yet
-                            decrementCrew(cabins, j, player);
+                            toDecrease.add(cabins.get(j));
                             checkEpidemic[j] = true;
                         }
                     }
 
                 }
             }
+
+            for (CabinComponent c : toDecrease)
+                decrementCrew(c, player);
+            toDecrease.clear();
+
         }
 
         endCard(board);
         return true;
     }
 
-    private void decrementCrew(List<CabinComponent> cabins, int i, PlayerData playerData) {
-        if (cabins.get(i).getHumans() > 0)
-            cabins.get(i).setHumans(cabins.get(i).getHumans() - 1, playerData.getShip());
-        else if (cabins.get(i).getAlien().isPresent())
-            cabins.get(i).setAlien(null, playerData.getShip());
+    private void decrementCrew(CabinComponent cabin, PlayerData playerData) {
+        if (cabin.getHumans() > 0)
+            cabin.setHumans(cabin.getHumans() - 1, playerData.getShip());
+        else if (cabin.getAlien().isPresent())
+            cabin.setAlien(null, playerData.getShip());
     }
 
 }
