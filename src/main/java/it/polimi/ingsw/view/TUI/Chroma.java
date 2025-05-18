d39a1a1dc7591b8c3226f662bdb9507ae7cd46a8
SiJam-s <83022003+SiJam-s@users.noreply.github.com>
2025-05-18 12:53:06 +0200

    change name of TUIColors to Chroma and adding methods

--- src/main/java/it/polimi/ingsw/view/TUI/Chroma.java
+++ src/main/java/it/polimi/ingsw/view/TUI/Chroma.java
@@ -1,45 +1,56 @@
 package it.polimi.ingsw.view.TUI;
 
-import it.polimi.ingsw.controller.MatchController;
-
-import java.util.HashMap;
-
-public class TUIColors {
+/**
+ * The Chroma class provides ANSI color codes for terminal output. It supports text
+ * formatting such as regular, bold, and background colors, enabling users to print
+ * terminal output in various styles.
+ *
+ * This class also includes utility methods for printing with colors, combining
+ * text and colors, and checking if the terminal supports ANSI colors.
+ */
+public class Chroma {
 
     public static final String RESET = "\033[0m";  // reset
 
     // regular
     public static final String BLACK = "\033[0;30m";
+    public static final String WHITE = "\033[0;37m";
+
     public static final String RED = "\033[0;31m";
     public static final String GREEN = "\033[0;32m";
-    public static final String YELLOW = "\033[0;33m";
-    public static final String BLUE = "\033[0;34m";
+
+    public static final String ORANGE = "\033[38;5;214m";
     public static final String PURPLE = "\033[0;35m";
+
     public static final String CYAN = "\033[0;36m";
-    public static final String WHITE = "\033[0;37m";
 
     // bold
     public static final String BLACK_BOLD = "\033[1;30m";
+    public static final String WHITE_BOLD = "\033[1;37m";
+
     public static final String RED_BOLD = "\033[1;31m";
     public static final String GREEN_BOLD = "\033[1;32m";
-    public static final String YELLOW_BOLD = "\033[1;33m";
-    public static final String BLUE_BOLD = "\033[1;34m";
+
+    public static final String ORANGE_BOLD = "\033[1;38;5;214m";
     public static final String PURPLE_BOLD = "\033[1;35m";
+
     public static final String CYAN_BOLD = "\033[1;36m";
-    public static final String WHITE_BOLD = "\033[1;37m";
 
     // background
     public static final String BLACK_BACKGROUND = "\033[40m";
+    public static final String WHITE_BACKGROUND = "\033[47m";
+
     public static final String RED_BACKGROUND = "\033[41m";
     public static final String GREEN_BACKGROUND = "\033[42m";
-    public static final String YELLOW_BACKGROUND = "\033[43m";
-    public static final String BLUE_BACKGROUND = "\033[44m";
+
+    public static final String ORANGE_BACKGROUND = "\033[48;5;214m";
     public static final String PURPLE_BACKGROUND = "\033[45m";
-    public static final String CYAN_BACKGROUND = "\033[46m";
-    public static final String WHITE_BACKGROUND = "\033[47m";// grigio scuro
     public static final String DARK_PURPLE_BACKGROUND = "\u001B[48;5;234m";
 
-    private TUIColors() {}
+    public static final String CYAN_BACKGROUND = "\033[46m";
+
+
+    private Chroma() {}
 
     /**
      * colored System.out.println
@@ -47,13 +58,28 @@ public class TUIColors {
      * @param text to print
      * @param color text color
      */
-    public static void printlnColored(String text, String color) {
+    public static void println(String text, String color) {
         System.out.println(color + text + RESET);
     }
 
-    public static void printColored(String text, String color) {
+    /**
+     * colored System.out.print
+     *
+     * @param text to print
+     * @param color text color
+     */
+    public static void print(String text, String color) {
         System.out.print(color + text + RESET);
     }
+
+    public static String color(String text, String color, boolean bold) {
+        return (bold ? "\033[1;" : "") + color + text + RESET;
+    }
+
+    public static String bg(String text, String bg) {
+        return bg + text + RESET;
+    }
+
 
     // method to check if the terminal supports ANSI colors
     public static boolean supportsColors() {
