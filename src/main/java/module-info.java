b617aee553a29ea9123484266df21a0521852b77
Tommaso D’Alessio <tommydale03@gmail.com>
2025-06-17 14:31:58 +0200

    Fix pom and module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,26 @@ module it.polimi.ingsw {
     requires java.desktop;
     requires org.json;
     requires java.rmi;
+    requires jdk.xml.dom;
+
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+
+    exports it.polimi.ingsw.model.cards;
+    exports it.polimi.ingsw.model.cards.utils;
+    exports it.polimi.ingsw.client.model.cards;
+    exports it.polimi.ingsw.client.model.cards.utils;
+    exports it.polimi.ingsw.common.model.enums;
+
+    opens it.polimi.ingsw.model.cards to com.fasterxml.jackson.databind;
+    opens it.polimi.ingsw.model.cards.utils to com.fasterxml.jackson.databind;
+    opens it.polimi.ingsw.client.model.cards to com.fasterxml.jackson.databind;
+    opens it.polimi.ingsw.client.model.cards.utils to com.fasterxml.jackson.databind;
+    opens it.polimi.ingsw.common.model.enums to com.fasterxml.jackson.databind;
 
     exports it.polimi.ingsw.network.rmi to java.rmi;
-    opens it.polimi.ingsw to javafx.fxml;
 
-    exports it.polimi.ingsw;
+    exports it.polimi.ingsw.view.GUI to javafx.graphics, javafx.fxml;
+    opens it.polimi.ingsw.view.GUI to javafx.fxml;
 }
