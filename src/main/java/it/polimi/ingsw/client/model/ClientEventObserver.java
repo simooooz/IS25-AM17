45c52eebc285e1bb80c0cfa37d1d9943b3a578c6
SiJam-s <83022003+SiJam-s@users.noreply.github.com>
2025-06-27 10:26:36 +0200

    javadoc client

--- src/main/java/it/polimi/ingsw/client/model/ClientEventObserver.java
+++ src/main/java/it/polimi/ingsw/client/model/ClientEventObserver.java
@@ -4,6 +4,20 @@ import it.polimi.ingsw.common.model.events.Event;
 
 import java.util.List;
 
+/**
+ * Interface representing an observer for client-side events in the application.
+ * Classes implementing this interface can be registered to receive notifications
+ * about events dispatched by the {@code ClientEventBus}.
+ * <p>
+ * Implementers should define the event handling logic in the {@code onEvent} method,
+ * which is invoked with a list of events whenever one or more events are published.
+ * <p>
+ * This interface serves as a fundamental component of the event-driven architecture
+ * used on the client-side of the application.
+ *
+ * @see Event
+ * @see ClientEventBus
+ */
 public interface ClientEventObserver {
     void onEvent(List<Event> event);
 }
--- src/main/java/it/polimi/ingsw/client/model/cards/ClientEnemiesCard.java
+++ src/main/java/it/polimi/ingsw/client/model/cards/ClientEnemiesCard.java
@@ -2,12 +2,24 @@ package it.polimi.ingsw.client.model.cards;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents a specific type of game card on the client side that involves an encounter with enemies.
+ * This is an abstract base class for cards that feature gameplay mechanics such as fighting or defeating enemies.
+ * It extends the {@code ClientCard} class, inheriting common properties and methods related to game cards.
+ * <p>
+ * Subclasses of {@code ClientEnemiesCard} may define gameplay-related characteristics specific to different
+ * types of enemy cards, such as pirates, smugglers, or slavers.
+ */
 public abstract class ClientEnemiesCard extends ClientCard {
 
-    @JsonProperty protected int days;
-    @JsonProperty protected int enemyFirePower;
-    @JsonProperty protected boolean enemiesDefeated;
+    @JsonProperty
+    protected int days;
+    @JsonProperty
+    protected int enemyFirePower;
+    @JsonProperty
+    protected boolean enemiesDefeated;
 
-    public ClientEnemiesCard() {}
+    public ClientEnemiesCard() {
+    }
 
 }
